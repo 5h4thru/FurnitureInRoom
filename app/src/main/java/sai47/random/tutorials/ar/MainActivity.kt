60921eac646bcce68774d8e6fdc228a0f8af47a8
Shathru <durgasaipreethamp@gmail.com>
2021-07-27 12:04:18 -0700

    Update colors and setup bottomsheet behavior

--- app/src/main/java/sai47/random/tutorials/ar/MainActivity.kt
+++ app/src/main/java/sai47/random/tutorials/ar/MainActivity.kt
@@ -1,11 +1,39 @@
 package sai47.random.tutorials.ar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet() {
+        val bottomSheetView = findViewById<View>(R.id.bottom_sheet)
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView)
+        bottomSheetBehavior.peekHeight = getPeekHeightForDevice()
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                bottomSheet.bringToFront()
+            }
+
+        })
+    }
+
+    /**
+     * Returns the peek height of the device running the app in pixels
+     */
+    private fun getPeekHeightForDevice(): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, BOTTOM_SHEET_PEEK_HEIGHT, resources.displayMetrics).toInt()
     }
 }
